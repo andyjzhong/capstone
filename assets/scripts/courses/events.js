06f3ad71525d0a11e93fc5b3343a81ed65f93746
andyjzhong <andy.zhong@libertymutual.com>
2017-06-06 13:35:31 -0400

    Added Element Detection
    Able to scrape the info from the edited fields and console log them.
    This will be critical for the update feature in a little while.

--- assets/scripts/courses/events.js
+++ assets/scripts/courses/events.js
@@ -15,8 +15,7 @@ const onIndexCourses = function () {
       $('.destroyButton').on('click', onDestroyCourse)
       $('.editButton').on('click', onEditCourse)
       $('.saveButton').on('click', onSaveCourse)
-      $('.saveButton').hide()
-      $('.editButton').show()
+      $('.cancelButton').on('click', onCancelCourse)
     })
     .catch(ui.actionFailure)
 }
@@ -29,8 +28,7 @@ const rerunIndex = function () {
       $('.destroyButton').on('click', onDestroyCourse)
       $('.editButton').on('click', onEditCourse)
       $('.saveButton').on('click', onSaveCourse)
-      $('.saveButton').hide()
-      $('.editButton').show()
+      $('.cancelButton').on('click', onCancelCourse)
     })
     .catch(ui.actionFailure)
 }
@@ -56,35 +54,96 @@ const onCreateCourse = function (event) {
     .then(rerunIndex)
 }
 
+// This function allows fields to be changed and displays the Edit UI
 const onEditCourse = function (event) {
   console.log('onEditCourse from Events.js ran.')
   const id = $(this).attr('data-id')
   $('td[data-id=' + id + ']').attr('contenteditable', 'true')
   $('tr[data-id=' + id + ']').css('border', '3px solid green')
-
-
-
-
-
-
-
-  // Fix this whn you get back. Also duplicated bottom.
-  $('button[class=editButton][data-id=' + id + ']').hide()
-  // $('.saveButton').show()
-  // $('.editButton').hide()
-  // const submitEdit = submitEditButton({})
-  // $('.blogpost-container[data-id=' + id + ']').append(submitEdit)
-  // $('#submit-edit').on('click', onSubmitEdit)
+  $('.submit-edit').on('click', onSaveCourse)
+  $('.cancel-edit').on('click', onCancelCourse)
 }
 
+// This function will ultimately be what actually fires the ajax call for updating this item.
 const onSaveCourse = function (event) {
   console.log('onSaveCourse from Events.js ran.')
+  // const title = $(this).parent().find('td')[0].innerHTML
+  // console.log(title)
+  const courseTitle = $(this).parent().parent().find('td.C1')[0].innerHTML
+  const courseSemester = $(this).parent().parent().find('td.C2')[0].innerHTML
+  const courseGrade = $(this).parent().parent().find('td.C3')[0].innerHTML
+  const courseCredits = $(this).parent().parent().find('td.C4')[0].innerHTML
+  console.log(courseTitle)
+  console.log(courseSemester)
+  console.log(courseGrade)
+  console.log(courseCredits)
+  // const courseSemester =
+  // const courseGrade =
+  // const courseCredits =
+  // const strippedTitle = courseTitle.replace(/(<([^>]+)>)/ig, '')
+  // const strippedSemester = courseSemester.replace(/(<([^>]+)>)/ig, '')
+  // const strippedGrade = courseGrade.replace(/(<([^>]+)>)/ig, '')
+  // const strippedCredits = courseCredits.replace(/(<([^>]+)>)/ig, '')
+  // const data = {
+  //   'course': {
+  //     'title': strippedTitle,
+  //     'semester': strippedSemester,
+  //     'grade': strippedGrade,
+  //     'credits': strippedCredits
+  //   }
+
+
+  // const id = $(this).attr('data-id')
+  // $('td[data-id=' + id + ']').attr('contenteditable', 'false')
+  // $('tr[data-id=' + id + ']').css('border', 'none')
+}
+
+
+
+// const onSubmitEdit = function (event) {
+//   event.preventDefault()
+//   const title = $(this).parent().find('h2')[0].innerHTML
+//   const blogpostContent = $(this).parent().find('p')[0].innerHTML
+//   const id = $(this).prev().attr('data-id')
+//   const strippedBlogContent = blogpostContent.replace(/(<([^>]+)>)/ig, '')
+//   const strippedBlogTitle = blogpostTitle.replace(/(<([^>]+)>)/ig, '')
+//   const data = {
+//     'blogpost': {
+//       'blogpostTitle': strippedBlogTitle,
+//       'blogpostContent': strippedBlogContent
+//     }
+//   }
+//   api.editBlog(data, id)
+//     .then(ui.editBlogSuccess)
+//     .then(() => {
+//       $('p[data-id=' + id + ']').attr('contenteditable', 'false')
+//       $('h2[data-id=' + id + ']').attr('contenteditable', 'false')
+//       $('.one-blogpost[data-id=' + id + ']').css('border', 'none')
+//       $('.blog-delete-button[data-id=' + id + ']').show()
+//       $('.blog-edit-button[data-id=' + id + ']').show()
+//       $('#submit-edit').remove()
+//       $('#cancel-edit').remove()
+//     })
+//     .catch(ui.editBlogFailure)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// This function reverts all fields back to being uneditable.
+const onCancelCourse = function (event) {
+  console.log('onCancelCourse from Events.js ran.')
   const id = $(this).attr('data-id')
   $('td[data-id=' + id + ']').attr('contenteditable', 'false')
   $('tr[data-id=' + id + ']').css('border', 'none')
-  $('button[class=saveButton][data-id=' + id + ']').hide()
-  // $('.saveButton').hide()
-  // $('.editButton').show()
 }
 
 const addHandlers = () => {
@@ -93,6 +152,7 @@ const addHandlers = () => {
   $('.destroyButton').on('click', onDestroyCourse)
   $('.editButton').on('click', onEditCourse)
   $('.saveButton').on('click', onSaveCourse)
+  $('.cancel-edit').on('click', onCancelCourse)
 }
 
 module.exports = {
